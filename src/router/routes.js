b97584be98b0e6624f876e1465e3e6950539b9a5
Alaa Rihan <alaa.addeen@gmail.com>
2023-03-07 21:35:06 +0200

    Remove dashboard page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,7 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "/dashboard", component: () => import("pages/Dashboard.vue") },
-    ],
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   // Always leave this as last one,
   // but you can also remove it
