de4553205d60ada81060b3e24e8d99ce93ce14f7
Alaa Rihan <alaa.addeen@gmail.com>
2024-02-18 18:51:27 +0200

    Add default db url

--- api/src/db_connection.rs
+++ api/src/db_connection.rs
@@ -3,7 +3,8 @@ use std::env;
 use sqlx::SqlitePool;
 
 pub async fn establish_connection() -> SqlitePool {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set");
+    let database_url =
+        env::var("DATABASE_URL").unwrap_or("sqlite://ResourceFile/webview_database.db".to_string());
     SqlitePool::connect(&database_url)
         .await
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
