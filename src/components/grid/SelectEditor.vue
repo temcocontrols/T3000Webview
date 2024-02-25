313118b048f733f8d4f1b15096c1a74f3bd0452a
Alaa Rihan <alaa.addeen@gmail.com>
2024-02-25 13:14:19 +0200

    Fix a bug in the ag-grid custom select editor

--- src/components/grid/SelectEditor.vue
+++ src/components/grid/SelectEditor.vue
@@ -1,61 +1,74 @@
-<script setup>
+<script>
 import { ref, toRaw, onMounted } from "vue";
-const props = defineProps({
-  params: {
-    type: Object,
+
+export default {
+  props: {
+    params: {
+      type: Object,
+    },
   },
-});
+  setup(props) {
+    const KEY_BACKSPACE = "Backspace";
+    const KEY_DELETE = "Delete";
+    const KEY_F2 = "F2";
 
-const KEY_BACKSPACE = "Backspace";
-const KEY_DELETE = "Delete";
-const KEY_F2 = "F2";
+    const value = ref(props.params.value);
 
-const value = ref(props.params.value);
+    const selectRef = ref(null);
 
-const selectRef = ref(null);
+    const selectOptions = ref(props.params.options);
+    const clearable = ref(props.params.clearable);
 
-const selectOptions = ref(props.params.options);
-const clearable = ref(props.params.clearable);
+    onMounted(() => {
+      setInitialState(props.params);
+    });
 
-onMounted(() => {
-  setInitialState(props.params);
-});
+    function selectFilter(val, update, abort) {
+      update(() => {
+        const keyword = val.toLowerCase();
+        selectOptions.value = props.params.options.filter(
+          (v) => v.toLowerCase().indexOf(keyword) > -1
+        );
+      });
+    }
 
-function selectFilter(val, update, abort) {
-  update(() => {
-    const keyword = val.toLowerCase();
-    selectOptions.value = props.params.options.filter(
-      (v) => v.toLowerCase().indexOf(keyword) > -1
-    );
-  });
-}
+    const getValue = () => {
+      return toRaw(value.value);
+    };
 
-const getValue = () => {
-  return toRaw(value.value);
+    const stopEditing = () => {
+      props.params.api.stopEditing();
+    };
+
+    function setInitialState(params) {
+      if (params.eventKey === KEY_BACKSPACE || params.eventKey === KEY_DELETE) {
+        // if backspace or delete pressed, we clear the cell
+        value.value = "";
+      } else if (params.eventKey?.length === 1) {
+        // if a letter was pressed, we show select popup
+        setTimeout(() => {
+          selectRef.value?.showPopup();
+          selectRef.value?.filter(params.eventKey);
+          selectRef.value?.updateInputValue(params.eventKey);
+        }, 100);
+      } else if (params.eventKey === KEY_F2) {
+        // if a letter was pressed, we show select popup
+        setTimeout(() => {
+          selectRef.value?.showPopup();
+        }, 100);
+      }
+    }
+    return {
+      value,
+      selectRef,
+      selectOptions,
+      clearable,
+      selectFilter,
+      stopEditing,
+      getValue,
+    };
+  },
 };
-
-const stopEditing = () => {
-  props.params.api.stopEditing();
-};
-
-function setInitialState(params) {
-  if (params.eventKey === KEY_BACKSPACE || params.eventKey === KEY_DELETE) {
-    // if backspace or delete pressed, we clear the cell
-    value.value = "";
-  } else if (params.eventKey?.length === 1) {
-    // if a letter was pressed, we show select popup
-    setTimeout(() => {
-      selectRef.value?.showPopup();
-      selectRef.value?.filter(params.eventKey);
-      selectRef.value?.updateInputValue(params.eventKey);
-    }, 100);
-  } else if (params.eventKey === KEY_F2) {
-    // if a letter was pressed, we show select popup
-    setTimeout(() => {
-      selectRef.value?.showPopup();
-    }, 100);
-  }
-}
 </script>
 
 <template>
