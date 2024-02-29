32b765cd0e476db5e47473f6d2fb7047d243ff9e
Alaa Rihan <alaa.addeen@gmail.com>
2024-03-01 00:51:45 +0200

    Catch errors instead of panic

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -7,9 +7,19 @@ pub mod modbus_register;
 pub mod server;
 pub mod user;
 
+#[repr(C)]
+pub enum RustError {
+    Ok = 0,
+    Error = 1,
+}
+
 #[no_mangle]
-pub extern "C" fn run_server() {
-    tokio::runtime::Runtime::new()
-        .unwrap()
-        .block_on(server::server_start());
+pub extern "C" fn run_server() -> RustError {
+    match tokio::runtime::Runtime::new() {
+        Ok(runtime) => match runtime.block_on(server::server_start()) {
+            Ok(_) => RustError::Ok,
+            Err(_) => RustError::Error,
+        },
+        Err(_) => RustError::Error,
+    }
 }
--- api/src/server.rs
+++ api/src/server.rs
@@ -17,31 +17,30 @@ use crate::app_state;
 use super::modbus_register::routes::modbus_register_routes;
 use super::user::routes::user_routes;
 
-pub async fn server_start() {
+pub async fn server_start() -> Result<(), Box<dyn std::error::Error>> {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
     dotenvy::dotenv().ok();
 
-    /* Check if the database file exists in Database/webview_database.db and if it's not then
-    copy it from ResourceFile/webview_database.db */
-    let database_url =
-        env::var("DATABASE_URL").unwrap_or("sqlite://Database/webview_database.db".to_string());
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "sqlite://Database/webview_database.db".to_string());
+
     let source_db_path = "ResourceFile/webview_database.db";
     let destination_db_path = database_url
         .strip_prefix("sqlite://")
-        .expect("Invalid database url");
+        .ok_or("Invalid database url")?;
 
-    println!("destination_db_path: {:?}", destination_db_path);
-
-    let destination_dir = Path::new(destination_db_path).parent().unwrap();
+    let destination_dir = Path::new(destination_db_path)
+        .parent()
+        .ok_or("Invalid destination database path")?;
 
     if !destination_dir.exists() {
-        fs::create_dir_all(destination_dir).expect("Failed to create directory");
+        fs::create_dir_all(destination_dir)?;
     }
 
     if !Path::new(destination_db_path).exists() {
-        fs::copy(source_db_path, destination_db_path).expect("Failed to copy database file");
+        fs::copy(source_db_path, destination_db_path)?;
     }
 
     let cors = CorsLayer::new()
@@ -53,27 +52,24 @@ pub async fn server_start() {
             Method::OPTIONS,
         ])
         .allow_headers(Any)
-        // allow requests from any origin
         .allow_origin(Any);
 
-    // build our application with a route
     let app = Router::new()
         .nest("/api", modbus_register_routes().merge(user_routes()))
         .layer(cors)
         .with_state(app_state::app_state().await)
         .fallback_service(routes_static());
 
-    let server_port = env::var("PORT").unwrap_or("9103".to_string());
+    let server_port = env::var("PORT").unwrap_or_else(|_| "9103".to_string());
 
-    // run our app with hyper
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", &server_port))
-        .await
-        .unwrap();
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", &server_port)).await?;
+
     println!("->> LISTENING on {:?}\n", listener.local_addr());
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
 
 async fn shutdown_signal() {
