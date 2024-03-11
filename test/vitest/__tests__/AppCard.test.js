522c91e749dd77c5dee8dd21f3caa934c2c7cf63
Alaa Rihan <alaa.addeen@gmail.com>
2024-03-11 23:09:16 +0200

    Add some unit tests

---
+++ test/vitest/__tests__/AppCard.test.js
@@ -0,0 +1,48 @@
+import AppCard from "../../../src/components/AppCard.vue";
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+installQuasarPlugin();
+
+describe("AppCard", () => {
+  it("renders correctly", () => {
+    const wrapper = mount(AppCard, {
+      props: {
+        app: {
+          id: "1",
+          name: "Test App",
+          user: {
+            id: "1",
+            name: "Test User",
+          },
+        },
+        showActions: true,
+      },
+    });
+
+    // Add the active class to simulate hover or active state
+    wrapper.find("#app-card-action-btn-1").element.classList.add("active");
+
+    // Check if the component is properly rendered
+    expect(wrapper.find(".app-card").exists()).toBe(true);
+
+    // Check if the action button is rendered when showActions is true
+    expect(wrapper.find("#app-card-action-btn-1").exists()).toBe(true);
+  });
+
+  it("handles actions correctly", async () => {
+    const wrapper = mount(AppCard, {
+      props: {
+        app: {
+          id: "1",
+          name: "Test App",
+          user: {
+            id: "1",
+            name: "Test User",
+          },
+        },
+        showActions: true,
+      },
+    });
+  });
+});
---
+++ test/vitest/__tests__/AppTabs.test.js
@@ -0,0 +1,24 @@
+import AppTabs from "../../../src/components/AppTabs.vue";
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+
+installQuasarPlugin();
+
+describe("AppTabs.vue", () => {
+  it("renders tabs with correct props and emits update event", async () => {
+    const wrapper = mount(AppTabs, {
+      props: {
+        modelValue: "inputs", // Set initial modelValue
+      },
+    });
+
+    // Assert initial state
+    expect(wrapper.find(".q-tabs .q-tab").exists()).toBe(true);
+
+    // Simulate tab click
+    await wrapper.find(".q-tabs .q-tab]").trigger("click"); // Click on mocked QTabs
+  });
+
+  // ... other tests
+});
