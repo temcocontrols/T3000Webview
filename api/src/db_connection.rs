bc77802cbea61399e07f42138334660e137e4e14
Alaa Rihan <alaa.addeen@gmail.com>
2024-04-25 20:15:32 +0200

    Improve connection config for database

--- api/src/db_connection.rs
+++ api/src/db_connection.rs
@@ -6,12 +6,12 @@ use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 
 pub async fn establish_connection() -> Result<DatabaseConnection, Box<dyn std::error::Error>> {
     let mut opt = ConnectOptions::new(DATABASE_URL.as_str());
-    opt.max_connections(10)
+    opt.max_connections(1)
         .min_connections(1)
-        .connect_timeout(Duration::from_secs(8))
-        .acquire_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .max_lifetime(Duration::from_secs(8))
+        // .connect_timeout(Duration::from_secs(8))
+        // .acquire_timeout(Duration::from_secs(8))
+        // .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(60 * 60 * 24))
         .sqlx_logging(false);
 
     Database::connect(opt).await.map_err(|error| error.into())
