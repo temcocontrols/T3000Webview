bbf4be1dc97d9643de4c73f897dac99d68ffdf63
Alaa Rihan <alaa.addeen@gmail.com>
2024-10-08 23:22:51 +0300

    Handle panic in the API

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,3 +1,4 @@
+use std::panic;
 use utils::copy_database_if_not_exists;
 
 pub mod app_state;
@@ -20,25 +21,33 @@ pub enum RustError {
 // Externally callable function to run the server for using from C++, returning a RustError.
 #[no_mangle]
 pub extern "C" fn run_server() -> RustError {
-    // Create a new Tokio runtime for asynchronous operations.
-    let runtime = match tokio::runtime::Runtime::new() {
-        Ok(rt) => rt,                      // Successfully created the runtime.
-        Err(_) => return RustError::Error, // Failed to create the runtime.
-    };
+    // Use panic::catch_unwind to catch any panic and prevent it from unwinding across FFI boundaries.
+    let result = panic::catch_unwind(|| {
+        // Create a new Tokio runtime for asynchronous operations.
+        let runtime = match tokio::runtime::Runtime::new() {
+            Ok(rt) => rt,                      // Successfully created the runtime.
+            Err(_) => return RustError::Error, // Failed to create the runtime.
+        };
 
-    // Run the server logic in a blocking thread within the Tokio runtime.
-    let result = runtime.block_on(async {
-        dotenvy::dotenv().ok(); // Load environment variables from a .env file, if it exists.
-        copy_database_if_not_exists().ok(); // Copy the database if it doesn't already exist.
-        match server::server_start().await {
-            Ok(_) => RustError::Ok, // Server started successfully.
-            Err(err) => {
-                // Handle server errors (log the error and return RustError::Error).
-                eprintln!("Server error: {:?}", err);
-                RustError::Error
+        // Run the server logic in a blocking thread within the Tokio runtime.
+        runtime.block_on(async {
+            dotenvy::dotenv().ok(); // Load environment variables from a .env file, if it exists.
+            copy_database_if_not_exists().ok(); // Copy the database if it doesn't already exist.
+            match server::server_start().await {
+                Ok(_) => RustError::Ok, // Server started successfully.
+                Err(err) => {
+                    // Handle server errors (log the error and return RustError::Error).
+                    eprintln!("Server error: {:?}", err);
+                    RustError::Error
+                }
             }
-        }
+        })
     });
 
-    result // Return the result of the server startup.
+    // If the closure inside catch_unwind ran without panicking, return the result.
+    // If a panic occurred, return an error.
+    match result {
+        Ok(res) => res,             // Normal execution, return the server result.
+        Err(_) => RustError::Error, // A panic occurred, return RustError::Error.
+    }
 }
