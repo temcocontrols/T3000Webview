e09c542e36c934a73631236d5edd4e1721c35d35
double <qiaonuo@outlook.com>
2024-11-15 17:22:10 +0800

    Cleaning up

--- src/components/ObjectTypes/WallExterior.vue
+++ src/components/ObjectTypes/WallExterior.vue
@@ -9,18 +9,6 @@ import { SVG } from '@svgdotjs/svg.js';
 export default defineComponent({
   name: "WallExteriorEl",
   props: {
-    // bgColor: {
-    //   type: String,
-    //   default: "#000",
-    // },
-    // strokeColor: {
-    //   type: String,
-    //   default: "#000",
-    // },
-    // strokeWidth: {
-    //   type: Number,
-    //   default: 19.5,
-    // },
     item: {
       type: Object,
       default: () => { }
@@ -30,12 +18,6 @@ export default defineComponent({
     onMounted(() => {
       console.log('WallExteriorEl', props);
       const draw = SVG().addTo(`#wall_${props?.item?.id}`).size('100%', '100%');
-
-      // draw.rect(100, 100).attr({
-      //   fill: props?.item?.settings?.bgColor,
-      //   stroke: props?.item?.settings?.strokeColor,
-      //   'stroke-width': props?.item?.settings?.strokeWidth
-      // });
 
       var line = draw.line(0, 0, props?.item?.translate[0], props?.item?.translate[1]);
       line.stroke({ color: props?.item?.settings?.strokeColor, width: props?.item?.settings?.strokeWidth });
