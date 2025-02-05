d34463d0acd545a10caa0265de84cab4c1efae97
double <qiaonuo@outlook.com>
2025-02-05 23:35:20 +0800

    Update Basic.Line

--- src/lib/T3000/Hvac/Basic/Basic.Line.ts
+++ src/lib/T3000/Hvac/Basic/Basic.Line.ts
@@ -1,19 +1,6 @@
 
 
-
-
-// import SDJS from "../SDJS/SDJS.Index";
-// import SDUI from "../SDUI/SDUI.Index";
-// import Basic from "./Basic.Index";
-// import GPP from "../gListManager";
-import $ from 'jquery';
-import HvacSVG from "../Helper/Helper.SVG.t2"
-
-
-import Path from "./Basic.Path";
-
-import Global from "./Basic.Global";
-
+import Path from "./Basic.Path"
 
 class Line extends Path {
 
@@ -21,38 +8,28 @@ class Line extends Path {
     super()
   }
 
-  // GetInstanceName(){
-  //   return "Line";
-  // }
+  SetPoints(
+    startX: number,
+    startY: number,
+    endX: number,
+    endY: number
+  ) {
+    console.log('= B.Line SetPoints startX,startY,endX,endY', startX, startY, endX, endY);
 
+    const pathCreator = this.PathCreator();
+    pathCreator.BeginPath();
+    pathCreator.MoveTo(startX, startY);
+    pathCreator.LineTo(endX, endY);
 
-  // Basic.Line = function () {
-  // },
-  //   Basic.Line.prototype = new Basic.Path,
-  //   Basic.Line.prototype.constructor = Basic.Line,
-  SetPoints(e, t, a, r) {
-    'use strict';
-    var i = this.PathCreator();
-    i.BeginPath(),
-      i.MoveTo(e, t),
-      i.LineTo(a, r);
-    var n = i.ToString();
-    this.SetPath(
-      n,
-      {
-        x: Math.min(e, a),
-        y: Math.min(t, r),
-        width: Math.abs(a - e),
-        height: Math.abs(r - t)
-      }
-    )
+    const pathString = pathCreator.ToString();
+
+    this.SetPath(pathString, {
+      x: Math.min(startX, endX),
+      y: Math.min(startY, endY),
+      width: Math.abs(endX - startX),
+      height: Math.abs(endY - startY),
+    });
   }
-
 }
 
-export default Line;
-
-
-
-
-// export default Basic.Line;
+export default Line
