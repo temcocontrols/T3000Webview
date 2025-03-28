a066a3a25182d71cbf68ba43c4487e05650a10bc
double <qiaonuo@outlook.com>
2025-03-28 22:54:25 +0800

    Update tool svg shape data

--- src/lib/T3000/Hvac/Opt/Tool/ToolSvgData.ts
+++ src/lib/T3000/Hvac/Opt/Tool/ToolSvgData.ts
@@ -252,17 +252,62 @@ class ToolSvgData {
 
     const temperature =
       `
-       <g id="layer1" stroke="#000000" stroke-width="1px" transform="translate(6,0)">
-    <!-- Updated yellow rectangle -->
-    <rect id="rect3012" height="29" width="6" y="5" x="21" fill="#ffff00"></rect>
-    <!-- Updated hexagon path -->
-    <path id="path3010" stroke-width="1.2px" fill="#ffffff" d="M 35,42.5 L 30,51 L 17,51 L 12,42.5 L 17,34 L 30,34 Z"></path>
-  </g>
-  <g id="g4141" transform="translate(17, -10) scale(2)">
-    <circle r="0.77312505" cy="28.129852" cx="6.4061227" id="path4137" style="fill: none; fill-opacity: 1; stroke: #000000; stroke-width: 0.5; stroke-miterlimit: 4; stroke-dasharray: none; stroke-opacity: 1;"></circle>
-    <path id="path4139" d="M 6.4061205,27.157211 V 23.65349" style="fill: none; fill-rule: evenodd; stroke: #000000; stroke-width: 0.5; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 4; stroke-dasharray: none; stroke-opacity: 1;"></path>
-  </g>
-
+      <g transform="scale(2.8,2.8) translate(6,0)">
+           <g
+      id="layer1"
+      stroke="#000000"
+      stroke-width="1px"
+      transform="translate(-10.804626,-9.2243756)"
+    >
+      <rect
+        id="rect3012"
+        height="14.505"
+        width="2.901"
+        y="9.9127998"
+        x="14.557"
+        fill="#ffff00"
+      />
+      <path
+        id="path3010"
+        stroke-width="1.2px"
+        transform="matrix(0.74098,0,0,0.74098,7.3047,15.679)"
+        fill="currentColor"
+        d="m 17.949,15.898 -3.1021,5.373 H 8.6427 L 5.5406,15.898 8.6427,10.525 h 6.2042 z"
+      />
+    </g>
+    <g id="g4141" transform="translate(-1.2033861,-8.1217684)">
+      <circle
+        r="0.77312505"
+        cy="28.129852"
+        cx="6.4061227"
+        id="path4137"
+        style="
+          fill: none;
+          fill-opacity: 1;
+          stroke: #000000;
+          stroke-width: 0.5;
+          stroke-miterlimit: 4;
+          stroke-dasharray: none;
+          stroke-opacity: 1;
+        "
+      />
+      <path
+        id="path4139"
+        d="M 6.4061205,27.157211 V 23.65349"
+        style="
+          fill: none;
+          fill-rule: evenodd;
+          stroke: #000000;
+          stroke-width: 0.5;
+          stroke-linecap: butt;
+          stroke-linejoin: miter;
+          stroke-miterlimit: 4;
+          stroke-dasharray: none;
+          stroke-opacity: 1;
+        "
+      />
+    </g>
+     </g>
     `;
 
 
@@ -271,12 +316,13 @@ class ToolSvgData {
 
   static BoilerSvgData() {
     const boiler = `
+    <g transform="scale(1.6,1.6) translate(3,3)">
     <path
       style="
-        fill: ##FillColor=#28c3c6##;
+        fill: currentColor;
         fill-opacity: 1;
         fill-rule: nonzero;
-        stroke:##LineColor=#000000##;
+        stroke: #000000;
         stroke-width: 1;
         stroke-linecap: butt;
         stroke-linejoin: miter;
@@ -284,8 +330,9 @@ class ToolSvgData {
         stroke-dasharray: none;
         stroke-opacity: 1;
       "
-      d="m 3.714,27.855 14.856,-12.999 0,-12.999 22.284,0 0,12.999 14.856,12.999 0,29.712 -51.996,0 z"
+      d="m 2,15 8,-7 0,-7 12,0 0,7 8,7 0,16 -28,0 z"
     ></path>
+    </g>
   `;
     return boiler;
   }
@@ -294,39 +341,39 @@ class ToolSvgData {
 
     const heatPump =
       `
-
+<g transform="scale(1.6,1.6) translate(3,3)">
       <g
-    class="heat-pump"
-    stroke-linejoin="round"
-    stroke="#000000"
-    transform="translate(30, 0)"
-    fill="#28c3c6"
-  >
-    <rect
-      class="inner"
-      height="60"
-      width="60"
-      y="0"
-      x="-30"
-      stroke-width="1.5"
-    ></rect>
-    <g
-      transform="matrix(2 0 0 2 -30 -1)"
-      stroke-linecap="round"
-      stroke-miterlimit="1"
-      stroke-width="1.5"
+      class="heat-pump"
+      stroke-linejoin="round"
+      stroke="#000"
+      transform="translate(39 -2.3842e-7)"
+      fill="currentColor"
     >
-      <path
-        d="m16.234 16.944 8.6837-6.894-8.6837-6.894v3.447h-13.152v6.894h13.152z"
-        fill="#ce2824"
-      ></path>
-      <path
-        d="m15.766 28.844-8.6837-6.894 8.6837-6.894v3.447h13.152v6.894h-13.152z"
-        fill="#3238db"
-      ></path>
+      <rect
+        class="inner"
+        height="27.718"
+        width="27.718"
+        y="2.141"
+        x="-36.859"
+        stroke-width="1.0868"
+      ></rect>
+      <g
+        transform="matrix(1.0276 0 0 1.0276 -39.441 -.44130)"
+        stroke-linecap="round"
+        stroke-miterlimit="1"
+        stroke-width="1.3509"
+      >
+        <path
+          d="m16.234 16.944 8.6837-6.894-8.6837-6.894v3.447h-13.152v6.894h13.152z"
+          fill="#ce2824"
+        ></path>
+        <path
+          d="m15.766 28.844-8.6837-6.894 8.6837-6.894v3.447h13.152v6.894h-13.152z"
+          fill="#3238db"
+        ></path>
+      </g>
     </g>
-  </g>
-
+ </g>
     `;
     return heatPump;
   }
@@ -334,25 +381,27 @@ class ToolSvgData {
   static PumpSvgData() {
     const pump =
       `
-      <g fill="#28c3c6" transform="translate(0,0)">
-    <g class="pump">
-      <circle
-        stroke="##LineColor=#000000##"
-        cy="30"
-        cx="30"
-        r="30"
-        class="pump-background"
-      />
-      <g transform="translate(30,30)">
-        <path
-          d="M -15,23.4107 V -23.4107 L 26.234,0 Z"
-          class="rotating-middle"
-          stroke="##LineColor=#000000##"
-          stroke-width="1.933"
+      <g transform="scale(2.5,2.5) translate(1,1)">
+      <g fill="currentColor" transform="translate(-5.4940996,-5.5390997)">
+      <g class="pump">
+        <circle
+          stroke="#000000"
+          cy="16"
+          cx="15.955"
+          r="9.9609003"
+          class="pump-background"
         />
+        <g transform="translate(16,16)">
+          <path
+            d="M -5,8.1369 V -8.1191 L 9.078,0.0091 Z"
+            class="rotating-middle"
+            stroke="#000000"
+            stroke-width="0.96719"
+          />
+        </g>
       </g>
     </g>
-  </g>
+    </g>
     `;
     return pump;
   }
@@ -361,34 +410,36 @@ class ToolSvgData {
 
     const valueThreeWay =
       `
-    <g transform="matrix(1,0,0,-1,0,60)">
-    <g
-      stroke-linejoin="round"
-      stroke="#000000"
-      class="twv"
-      fill="currentColor"
-    >
-      <g stroke-linecap="round">
-        <path d="M 30,30 9,44.7882 V 15.2118 Z" />
-        <path d="M 30,30 51,44.7882 V 15.2118 Z" />
-        <path d="M 30,30 18.2118,9 H 41.7882 Z" />
-      </g>
-      <g class="blocker-wrapper">
-        <g class="blocker" fill="#000000">
-          <path
-            stroke-width="1.5"
-            d="m 30,30 a 15.1515,15.1515 0 0 0 -10.303,4.0909 l 2.75,4.75 A 9.8182,9.8182 0 0 1 30,35.3 9.8182,9.8182 0 0 1 39.5758,38.525 l 2.75,-4.75 A 15.1515,15.1515 0 0 0 30,30 Z"
-          />
+      <g transform="scale(1.8,1.8) translate(5,4)">
+    <g transform="matrix(1,0,0,-1,-4.3773334,22.894063)">
+      <g
+        stroke-linejoin="round"
+        stroke="#000000"
+        class="twv"
+        fill="currentColor"
+      >
+        <g stroke-linecap="round">
+          <path d="M 16,16 4.925,22.3941 V 9.6061 Z" />
+          <path d="m 16,16 11.075,6.3941 V 9.6061 Z" />
+          <path d="M 16,16 9.606,4.925 h 12.788 z" />
+        </g>
+        <g class="blocker-wrapper">
+          <g class="blocker" fill="#000000" transform="translate(16,6)">
+            <path
+              stroke-width="0.80716"
+              d="m 0,-9.6108 a 8.8788,8.8788 0 0 0 -6.0273,2.375 l 1.6172,2.8008 A 5.7576,5.7576 0 0 1 0,-6.4897 5.7576,5.7576 0 0 1 4.4062,-4.4292 L 6.0293,-7.2397 A 8.8788,8.8788 0 0 0 0,-9.6108 Z"
+            />
+          </g>
         </g>
       </g>
+      <path
+        d="M 6.7797,10.576 V 21.084"
+        stroke="#000000"
+        stroke-width="1px"
+        fill="none"
+      />
     </g>
-    <path
-      d="M 12.5,17.5 V 35"
-      stroke="#000000"
-      stroke-width="1.5px"
-      fill="none"
-    />
-  </g>
+      </g>
     `;
 
     return valueThreeWay;
@@ -398,26 +449,29 @@ class ToolSvgData {
 
     const valueTwoWay =
       `
+
+      <g transform="scale(2.2,2.2) translate(2,6)">
     <g
-    stroke-linejoin="round"
-    fill-rule="nonzero"
-    transform="translate(0,0) scale(2)"
-    stroke="#000000"
-    stroke-linecap="round"
-    stroke-dasharray="none"
-    stroke-miterlimit="4"
-    stroke-width="1"
-    fill="currentColor"
-  >
-    <path
-      d="M 10.068,7.9344 -1.007,14.3285 V 1.5405 Z"
-      transform="translate(5.3992957,6.473236)"
-    />
-    <path
-      d="M 10.068,7.9344 -1.007,14.3285 V 1.5405 Z"
-      transform="matrix(-1,0,0,1,25.341517,6.354592)"
-    />
-  </g>
+      stroke-linejoin="round"
+      fill-rule="nonzero"
+      transform="translate(-3.8922957,-7.3951286)"
+      stroke="#000000"
+      stroke-linecap="round"
+      stroke-dasharray="none"
+      stroke-miterlimit="4"
+      stroke-width="1"
+      fill="currentColor"
+    >
+      <path
+        d="M 10.068,7.9344 -1.007,14.3285 V 1.5405 Z"
+        transform="translate(5.3992957,6.473236)"
+      />
+      <path
+        d="M 10.068,7.9344 -1.007,14.3285 V 1.5405 Z"
+        transform="matrix(-1,0,0,1,25.341517,6.354592)"
+      />
+    </g>
+    </g>
     `;
 
     return valueTwoWay;
@@ -431,40 +485,42 @@ class ToolSvgData {
 
     const fan =
       `
-    <g fill="currentColor" transform="translate(0, 0) scale(2)">
-    <g class="fan">
-      <g id="g8">
-        <path
-          class="fan-background"
-          d="m12.297 5.154c-6.117 0-11.066 4.9492-11.066 11.066 0 6.117 4.9492 11.066 11.066 11.066 5.7137 0 10.408-4.3091 11.003-9.8647h7.525v-12.268h-18.148c-0.12601-0.0043-0.25236 0-0.37941 0z"
-          fill-rule="evenodd"
-          stroke="#000"
-          stroke-width="1px"
-          fill="inherit"
-        />
-      </g>
-      <g id="g4148" transform="translate(12.464 16.395)">
-        <g
-          id="g4162"
-          stroke="#000"
-          stroke-width="1.5"
-          fill="none"
-          class="rotating-middle"
-        >
-          <path id="use3009" d="m-1.8112-3.9787 3.6227-4.4971" />
-          <path id="use3011" d="m0.87376-4.2835 5.5741-1.5089" />
-          <path id="use3013" d="m3.2243-2.9521 5.3965 2.0557" />
-          <path id="use3015" d="m4.3433-0.4935 3.1575 4.835" />
-          <path id="use3017" d="m3.8043 2.1547-0.28747 5.7676" />
-          <path id="use3019" d="m1.8113 3.9787-3.6227 4.4971" />
-          <path id="use3021" d="m-0.87324 4.2837-5.5741 1.5089" />
-          <path id="use3023" d="m-3.2242 2.9527-5.3965-2.0559" />
-          <path id="use3025" d="m-4.3432 0.4935-3.158-4.8353" />
-          <path id="use3027" d="m-3.8032-2.1541 0.2875-5.7676" />
+      <g transform="scale(1.6,1.6) translate(2,8)">
+   <g fill="currentColor" transform="matrix(1, 0, 0, 1, -0.231, -4.151388)">
+      <g class="fan">
+        <g id="g8">
+          <path
+            class="fan-background"
+            d="m12.297 5.154c-6.117 0-11.066 4.9492-11.066 11.066 0 6.117 4.9492 11.066 11.066 11.066 5.7137 0 10.408-4.3091 11.003-9.8647h7.525v-12.268h-18.148c-0.12601-0.0043-0.25236 0-0.37941 0z"
+            fill-rule="evenodd"
+            stroke="#000"
+            stroke-width="1px"
+            fill="inherit"
+          />
+        </g>
+        <g id="g4148" transform="translate(12.464 16.395)">
+          <g
+            id="g4162"
+            stroke="#000"
+            stroke-width="1.5"
+            fill="none"
+            class="rotating-middle"
+          >
+            <path id="use3009" d="m-1.8112-3.9787 3.6227-4.4971" />
+            <path id="use3011" d="m0.87376-4.2835 5.5741-1.5089" />
+            <path id="use3013" d="m3.2243-2.9521 5.3965 2.0557" />
+            <path id="use3015" d="m4.3433-0.4935 3.1575 4.835" />
+            <path id="use3017" d="m3.8043 2.1547-0.28747 5.7676" />
+            <path id="use3019" d="m1.8113 3.9787-3.6227 4.4971" />
+            <path id="use3021" d="m-0.87324 4.2837-5.5741 1.5089" />
+            <path id="use3023" d="m-3.2242 2.9527-5.3965-2.0559" />
+            <path id="use3025" d="m-4.3432 0.4935-3.158-4.8353" />
+            <path id="use3027" d="m-3.8032-2.1541 0.2875-5.7676" />
+          </g>
         </g>
       </g>
     </g>
-  </g>
+    </g>
     `;
 
     return fan;
@@ -474,7 +530,9 @@ class ToolSvgData {
 
     const coolingCoil =
       `
-       <g stroke="#000" transform="matrix(1, 0, 0, 1, -6.058399, -1.3729)">
+
+      <g transform="scale(2,2) translate(5.5,0)">
+         <g stroke="#000" transform="matrix(1, 0, 0, 1, -6.058399, -1.3729)">
       <rect
         id="rect4367"
         height="27.246"
@@ -516,6 +574,7 @@ class ToolSvgData {
         fill="none"
       />
     </g>
+    </g>
     `;
 
     return coolingCoil;
@@ -525,7 +584,9 @@ class ToolSvgData {
 
     const heatingCoil =
       `
-    <defs id="defs3">
+
+      <g transform="scale(2,2) translate(5.5,0)">
+     <defs id="defs3">
       <rect id="heating-coil-rect" height="27.246" width="17.868" x="0" y="0" />
     </defs>
     <g stroke="#000000" id="g15" transform="translate(-6.129206,-1.5327875)">
@@ -566,6 +627,7 @@ class ToolSvgData {
         fill="none"
       />
     </g>
+    </g>
     `;
 
     return heatingCoil;
@@ -575,6 +637,7 @@ class ToolSvgData {
 
     const filter =
       `
+      <g transform="scale(2,2) translate(9,0)">
     <g
       stroke="#000"
       stroke-width="1"
@@ -599,6 +662,7 @@ class ToolSvgData {
         fill="none"
       />
     </g>
+       </g>
     `;
 
     return filter;
@@ -608,6 +672,8 @@ class ToolSvgData {
 
     const humidifier =
       `
+
+      <g transform="scale(2.2,2.2) translate(5,2)">
     <g
       class="duct-humidifier"
       transform="matrix(1, 0, 0, 1, -3.324199, -7.877)"
@@ -668,6 +734,7 @@ class ToolSvgData {
         fill="currentColor"
       />
     </g>
+     </g>
     `;
 
     return humidifier;
@@ -684,6 +751,8 @@ class ToolSvgData {
     const Humidity =
       `
 
+
+      <g transform="scale(2.5,2.5) translate(8,0)">
     <rect
       style="
         fill: #00d1ff;
@@ -751,6 +820,7 @@ class ToolSvgData {
         />
       </g>
     </g>
+     </g>
     `;
 
     return Humidity;
@@ -760,6 +830,7 @@ class ToolSvgData {
 
     const pressure =
       `
+      <g transform="scale(1.5,1.5) translate(6,6)">
           <path
       style="fill: currentColor; stroke: #000000; stroke-width: 1.29098px"
       d="M 21.101011,20.624556 17.76371,26.404938 H 11.089107 L 7.7518058,20.624556 11.089107,14.844174 h 6.674603 z"
@@ -860,6 +931,7 @@ class ToolSvgData {
         "
       />
     </g>
+    </g>
       `;
 
     return pressure;
@@ -869,6 +941,7 @@ class ToolSvgData {
 
     const damper =
       `
+      <g transform="scale(2,2) translate(10,0)">
     <rect
       class="damper"
       height="27.5"
@@ -936,6 +1009,7 @@ class ToolSvgData {
     >
       <path stroke-width="1.06273px" d="m 16.298,2.2402 v 28.442" />
     </g>
+    </g>
     `;
     return damper;
   }
@@ -948,6 +1022,7 @@ class ToolSvgData {
 
     const thermalWheel =
       `
+      <g transform="scale(1,1) translate(18,10)">
     <g stroke="#000000" fill="currentColor" transform="translate(-2.370089)">
       <rect
         class="thermal-wheel-background"
@@ -971,6 +1046,7 @@ class ToolSvgData {
         <line x1="3" x2="28.5" y1="32" y2="32" />
         <line x1="3" x2="28.5" y1="36" y2="36" />
       </g>
+    </g>
     </g>
     `;
 
