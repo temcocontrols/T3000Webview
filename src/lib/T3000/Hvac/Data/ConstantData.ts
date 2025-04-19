2c2c01ae9f15b0173ce0e0c521499d10af2e92a6
double <qiaonuo@outlook.com>
2025-04-20 01:42:10 +0800

    Clean up Model

--- src/lib/T3000/Hvac/Data/ConstantData.ts
+++ src/lib/T3000/Hvac/Data/ConstantData.ts
@@ -1,15 +1,15 @@
 
 
-class ConstantData {
-  public static LineType = {
-    LINE: 1
-  }
+// class ConstantData {
+//   public static LineType = {
+//     LINE: 1
+//   }
 
-  public static DrawingObjectBaseClass = {
-    SHAPE: 0,
-    LINE: 1,
-    CONNECTOR: 3
-  }
-}
+//   public static DrawingObjectBaseClass = {
+//     SHAPE: 0,
+//     LINE: 1,
+//     CONNECTOR: 3
+//   }
+// }
 
-export default ConstantData
+// export default ConstantData
