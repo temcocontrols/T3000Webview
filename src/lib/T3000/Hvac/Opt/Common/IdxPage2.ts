1b3ef804728560cdf8dec601abfcd3a3781fed74
double <qiaonuo@outlook.com>
2025-04-25 21:31:30 +0800

    Update socket for index page2

--- src/lib/T3000/Hvac/Opt/Common/IdxPage2.ts
+++ src/lib/T3000/Hvac/Opt/Common/IdxPage2.ts
@@ -1,7 +1,10 @@
-import { isBuiltInEdge, T3_Types } from "../../Data/T3Data";
 import Hvac from "../../Hvac";
 import IdxUtils from "./IdxUtils";
-
+import {
+  globalNav, user, emptyLib, library, appState, rulersGridVisible, isBuiltInEdge, documentAreaPosition, savedNotify,
+  viewportMargins, viewport, locked, deviceModel, T3_Types, emptyProject, undoHistory, redoHistory, moveable, deviceAppState,
+  globalMsg, loadSettings
+} from "../../Data/T3Data"
 
 class IdxPage2 {
 
@@ -9,6 +12,8 @@ class IdxPage2 {
 
   // Access Quasar framework instance
   public $q: any;
+  public getPanelsInterval: any;
+  public autoSaveInterval: any;
 
   initPage() {
     Hvac.WebClient.initMessageHandler();
@@ -17,7 +22,7 @@ class IdxPage2 {
     //  this.restoreAppState();
     //  this.setDocMarginOffset();
     //  this.initPanzoom();
-    //  this.initMessageClient();
+    this.initMessageClient();
     //  this.initScorller();
     //  this.initAutoSaveInterval();
     //  this.initWindowListener();
@@ -72,6 +77,113 @@ class IdxPage2 {
     }
 
     console.log('= Idx T3UpdateEntryField to T3 before, after', tempFieldBefore, fieldVal);
+  }
+
+  // Set intervals for fetching panel and entry data if in a webview
+  initFetchPanelEntryInterval() {
+    if (!this.webview?.postMessage) {
+      return;
+    }
+
+    this.getPanelsInterval = setInterval(() => {
+      Hvac.WebClient.GetPanelsList();
+    }, 10000);
+
+    setInterval(function () {
+      const lkdEntries = IdxUtils.getLinkedEntries();
+
+      if (lkdEntries.length <= 0) {
+        return;
+      }
+
+      const etries = lkdEntries.map((ii) => {
+        return {
+          panelId: ii.t3Entry.pid,
+          index: ii.t3Entry.index,
+          type: T3_Types[ii.t3Entry.type],
+        };
+      });
+
+      Hvac.WebClient.GetEntries(null, null, etries);
+
+    }, 10000);
+  }
+
+  initMessageClient() {
+    // Built-in explorer
+
+    if (isBuiltInEdge.value) {
+      Hvac.WebClient.GetInitialData();
+      Hvac.WebClient.GetPanelsList();
+      this.initFetchPanelEntryInterval();
+      return;
+    }
+
+    // External explorer
+    // If accessed from an external browser
+    if (!isBuiltInEdge.value) {
+      this.initExternalBrowserOpt();
+      return;
+    }
+  }
+
+  initExternalBrowserOpt() {
+
+    if (isBuiltInEdge.value) return;
+
+    Hvac.WsClient.initQuasar(this.$q);
+
+    // connect to the ws://localhost:9104 websocket server
+    Hvac.WsClient.connect();
+
+    // check if need to show the device list dialog
+    setTimeout(() => {
+      const currentDevice = Hvac.DeviceOpt.getCurrentDevice();
+      if (!currentDevice) {
+        deviceModel.value.active = true;
+      }
+      else {
+        deviceModel.value.active = false;
+        deviceModel.value.data = currentDevice;
+
+        console.log('= IdxPage load from local storage', currentDevice);
+
+        // load device appstate
+        //Hvac.DeviceOpt.refreshDeviceAppState();
+        Hvac.WsClient.GetInitialData(currentDevice.deviceId, currentDevice.graphic, true);
+
+        // console.log('=== indexPage.currentDevice load from local storage', currentDevice);
+        // console.log('=== indexPage.deviceModel changed', deviceModel.value);
+      }
+    }, 1000);
+
+    setInterval(function () {
+      const linkedEntries = IdxUtils.getLinkedEntries();
+      if (linkedEntries.length === 0) return;
+
+      const data = linkedEntries.map((ii) => {
+        return {
+          panelId: ii.t3Entry.pid,
+          index: ii.t3Entry.index,
+          type: T3_Types[ii.t3Entry.type],
+        };
+      });
+
+      Hvac.WsClient.GetEntries(data);
+
+      /*
+      window.chrome?.webview?.postMessage({
+        action: 6, // GET_ENTRIES
+        data: getLinkedEntries().map((ii) => {
+          return {
+            panelId: ii.t3Entry.pid,
+            index: ii.t3Entry.index,
+            type: T3_Types[ii.t3Entry.type],
+          };
+        }),
+      });
+      */
+    }, 10000);
   }
 }
 
