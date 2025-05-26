f48e9f2250857bf32d5349950d65e0b7f0024508
double <qiaonuo@outlook.com>
2025-05-26 14:39:48 +0800

    Update config

--- quasar.config.js
+++ quasar.config.js
@@ -28,7 +28,7 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
-      'antd'
+      //'antd'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
