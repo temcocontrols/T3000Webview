4339fde0eb741090f7841a8aa1d91d428c10db20
double <qiaonuo@outlook.com>
2025-12-24 21:57:29 +0800

    Update hvac lib

---
+++ src/lib/t3-hvac/Hvac.ts
@@ -0,0 +1,45 @@
+
+import PageMain from "./Page/P.Main";
+import T3Opt from "./Doc/T3Opt";
+import KeyInsertOpt from "./Opt/Common/KeyInsertOpt";
+import DeviceOpt from "./Opt/Socket/DeviceOpt";
+import WebSocketClient from "./Opt/Socket/WebSocketClient";
+import IdxPage from "./Opt/Common/IdxPage";
+import WebViewClient from "./Opt/Webview2/WebViewClient";
+import QuasarUtil from "./Opt/Quasar/QuasarUtil";
+import LsOpt from "./Opt/Common/LsOpt";
+import DocUtil from "./Doc/DocUtil";
+import IdxPage2 from "./Opt/Common/IdxPage2";
+import IdxUtils from "./Opt/Common/IdxUtils";
+
+const Hvac = {
+  PageMain: new PageMain(),
+  UI: null as any, // Will be initialized after module loading to avoid circular dependency
+  // Doc: new DocUtil(),
+  KiOpt: new KeyInsertOpt(),
+  QuasarUtil: new QuasarUtil(),
+  LsOpt: new LsOpt(),
+  DeviceOpt: new DeviceOpt(),
+  WsClient: new WebSocketClient(),
+  IdxPage: new IdxPage(),
+  WebClient: null as any, // Will be initialized after module loading
+  IdxUtils: IdxUtils, // Static class reference
+  IdxPage2: new IdxPage2(),
+}
+
+// Inject dependencies into WebSocketClient to break circular dependency
+Hvac.WsClient.setDependencies(Hvac.DeviceOpt, Hvac.IdxPage, Hvac.QuasarUtil);
+
+// Initialize T3Opt after module loading to avoid circular dependency
+Hvac.UI = new T3Opt();
+
+// Initialize WebViewClient after module loading to avoid circular dependency
+Hvac.WebClient = new WebViewClient();
+
+// Set up IdxUtils dependencies first (static class)
+Hvac.IdxUtils.setDependencies(Hvac.WebClient, Hvac.DeviceOpt, Hvac.WsClient);
+
+// Inject dependencies into WebViewClient to break circular dependency
+Hvac.WebClient.setDependencies(Hvac.DeviceOpt, Hvac.IdxPage, Hvac.IdxUtils);
+
+export default Hvac
