61bd425c7add15757e4e1daa3ecad2db45575044
double <qiaonuo@outlook.com>
2026-01-28 10:19:08 +0800

    Update version

--- src/lib/vue/T3000/Hvac/Data/Store/StateStore.ts
+++ src/lib/vue/T3000/Hvac/Data/Store/StateStore.ts
@@ -166,9 +166,9 @@ class StateStore {
   public readonly globalMsg: Ref<GlobalMsgModel[]> = ref([]);
 
   // Settings and version
-  public readonly devVersion: Ref<string> = ref("V:26.0122.01");
+  public readonly devVersion: Ref<string> = ref("V:26.0128.01");
   public readonly localSettings: Ref<LocalSettings> = ref({
-    version: "V:26.0122.01",
+    version: "V:26.0128.01",
     transform: 0
   });
 
