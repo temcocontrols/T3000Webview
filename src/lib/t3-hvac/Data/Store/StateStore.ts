2003352bd975fa686583bc2e11637987544cfaf8
double <qiaonuo@outlook.com>
2026-01-30 15:58:49 +0800

    Update version no

--- src/lib/t3-hvac/Data/Store/StateStore.ts
+++ src/lib/t3-hvac/Data/Store/StateStore.ts
@@ -174,9 +174,9 @@ class StateStore {
   public readonly globalMsg: Ref<GlobalMsgModel[]> = ref([]);
 
   // Settings and version
-  public readonly devVersion: Ref<string> = ref("V:26.0130.01");
+  public readonly devVersion: Ref<string> = ref("V:26.0130.02");
   public readonly localSettings: Ref<LocalSettings> = ref({
-    version: "V:26.0130.01",
+    version: "V:26.0130.02",
     transform: 0
   });
 
