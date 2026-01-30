1cb60adbb9f49785a6940bab298d7afcabb3e56c
double <qiaonuo@outlook.com>
2026-01-30 10:01:16 +0800

    Update version no

--- src/lib/t3-hvac/Data/T3Data.ts
+++ src/lib/t3-hvac/Data/T3Data.ts
@@ -679,9 +679,9 @@ export const moveable = ref(null); // Reference to the moveable component instan
 // export const globalMsg = ref({ type: "info", message: "", isShow: false, msgType: "" });// Global message state
 export const globalMsg = ref<GlobalMsgModel[]>([]);
 
-export const devVersion = ref("V:26.0129.01");
+export const devVersion = ref("V:26.0130.01");
 
-export const localSettings = ref({ version: "V:26.0129.01", transform: 0 });
+export const localSettings = ref({ version: "V:26.0130.01", transform: 0 });
 
 export const T3Data = {
   deviceList: ref([]),
